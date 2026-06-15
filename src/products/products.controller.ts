cce5755430b2545510a2321ba4fbc592f79063bb
Dragon <167077773+Dragon-Palestine@users.noreply.github.com>
2026-06-15 13:36:54 +0300

    add service

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -5,79 +5,47 @@ import {
   Put,
   Body,
   Param,
-  NotFoundException,
   Delete,
   ParseIntPipe,
 } from '@nestjs/common';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { UpdateProductDto } from './dtos/update-product.dto';
+import { ProductsService } from './products.service';
 
-type int = number;
-type ProductType = { id: int; title: string; price: int };
+// type ProductType = { id: number; title: string; price: number };
 
 @Controller('api/products')
 export class ProductsController {
-  private products: ProductType[] = [
-    { id: 1, title: 'book', price: 10 },
-    { id: 2, title: 'pen', price: 5 },
-    { id: 3, title: 'lap', price: 400 },
-  ];
+  private productsService: ProductsService = new ProductsService();
 
+  // POST: ~/api/products
   @Post()
-  public createNewProduct(
-    @Body()
-    body: CreateProductDto,
-  ): ProductType {
-    const newProduct: ProductType = {
-      id: this.products.length + 1,
-      title: body.title,
-      price: body.price,
-    };
-    this.products.push(newProduct);
-    return newProduct;
+  public createNewProduct(@Body() body: CreateProductDto) {
+    return this.productsService.create(body);
   }
   // GET: ~/api/products
   @Get()
   public getAllProducts() {
-    return this.products;
+    return this.productsService.getAll();
   }
-  /**
-   *
-   * @param id
-   * @returns ProductType
-   */
-  // GET: ~/api/products
+
+  // GET: ~/api/products/:id
   @Get(':id')
-  public getSingleProduct(@Param('id', ParseIntPipe) id: number): ProductType {
-    const product: ProductType | undefined = this.products.find(
-      (p) => p.id === id,
-    );
-    if (!product) throw new NotFoundException('this id is not in !!');
-    return product;
+  public getSingleProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.getSingleProduct(id);
   }
-  /**
-   *
-   * @param id
-   * @param body
-   * @returns
-   */
-  // PUT: ~/api/products
+  // PUT: ~/api/products/:id
   @Put(':id')
   public updateSingleProduct(
     @Param('id', ParseIntPipe) id: number,
     @Body() body: UpdateProductDto,
   ) {
-    const product = this.products.find((p) => p.id === id);
-    if (!product) throw new NotFoundException('this id is not in !!');
-    console.log(body);
-    return body;
+    return this.productsService.update(id, body);
   }
 
-  // DELETE: ~/api/products
+  // DELETE: ~/api/products/:id
   @Delete(':id')
   public deleteSingleProduct(@Param('id', ParseIntPipe) id: number) {
-    const product = this.products.find((p) => p.id === id);
-    if (!product) throw new NotFoundException('this id is not in !!');
-    return product;
+    return this.productsService.delete(id);
   }
 }
---
+++ src/products/products.service.ts
@@ -0,0 +1,75 @@
+import { NotFoundException } from '@nestjs/common';
+import { UpdateProductDto } from './dtos/update-product.dto';
+import { CreateProductDto } from './dtos/create-product.dto';
+
+type ProductType = { id: number; title: string; price: number };
+
+export class ProductsService {
+  private products: ProductType[] = [
+    { id: 1, title: 'book', price: 10 },
+    { id: 2, title: 'pen', price: 5 },
+    { id: 3, title: 'lap', price: 400 },
+  ];
+
+  /**
+   *
+   * @param createProductDto
+   * @returns
+   */
+
+  public create(createProductDto: CreateProductDto): ProductType {
+    const newProduct: ProductType = {
+      id: this.products.length + 1,
+      title: createProductDto.title,
+      price: createProductDto.price,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+
+  /**
+   *
+   * @returns
+   */
+
+  public getAll() {
+    return this.products;
+  }
+  /**
+   *
+   * @param id
+   * @returns ProductType
+   */
+  public getSingleProduct(id: number): ProductType {
+    const product: ProductType | undefined = this.products.find(
+      (p) => p.id === id,
+    );
+    if (!product) throw new NotFoundException('this id is not in !!');
+    return product;
+  }
+  /**
+   *
+   * @param id
+   * @param updateProductDto
+   * @returns
+   */
+  public update(
+    id: number,
+    updateProductDto: UpdateProductDto,
+  ): UpdateProductDto {
+    const product = this.products.find((p) => p.id === id);
+    if (!product) throw new NotFoundException('this id is not in !!');
+    console.log(updateProductDto);
+    return updateProductDto;
+  }
+  /**
+   *
+   * @param id
+   * @returns
+   */
+  public delete(id: number): string {
+    const product = this.products.find((p) => p.id === id);
+    if (!product) throw new NotFoundException('this id is not in !!');
+    return 'product deleted';
+  }
+}
